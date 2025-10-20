e95dae92759a3a6fe59a94288f7847a204f5419a
SA Omene <eserogheneo@gmail.com>
2025-10-20 13:33:59 -0500

    Update MoveOption.cs
    Fixed bug for disabling moves that cost more elixir than what the player has

--- Game Design/UI/Battle UI/Options UI/MoveOption.cs
+++ Game Design/UI/Battle UI/Options UI/MoveOption.cs
@@ -22,7 +22,7 @@ public class MoveOption : MonoBehaviour
     //private variables
     private string _moveName;
 
-    public void Start()
+    public void OnEnable()
     {
         InitializeMoveOption();
     }
@@ -55,10 +55,7 @@ public class MoveOption : MonoBehaviour
 
     private void InitializeMoveOption()
     {
-        MovePowerText.text = "000";
-        MoveAccuracyText.text = "000";
-        MoveTypeText.text = "";
-        MoveDescriptionText.text = "";
+        ClearMoveLayout();
 
         foreach (Move move in Player.Instance().BattleMoves)
         {
@@ -67,7 +64,7 @@ public class MoveOption : MonoBehaviour
                 Button moveButton = Instantiate(MoveButtonPrefab, MoveLayout).GetComponent<Button>();
                 TextMeshProUGUI moveButtonText = moveButton.GetComponentInChildren<TextMeshProUGUI>();
                 moveButtonText.text = move.Name + "\n" + move.EP.ToString() + " EP";
-                moveButton.interactable = Player.Instance().BaseStats.Elx > move.EP;
+                moveButton.interactable = Player.Instance().BaseStats.Elx >= move.EP;
                 moveButton.onClick.AddListener(() =>
                 {
                     _moveName = move.Name;
@@ -95,5 +92,17 @@ public class MoveOption : MonoBehaviour
         MoveAccuracyText.text = ((int)(move.Accuracy * 100)).ToString();
         MoveTypeText.text = move.Type.ToString();
         MoveDescriptionText.text = move.Description;
+    }
+
+    private void ClearMoveLayout()
+    {
+        _moveName = null;
+        MovePowerText.text = "000";
+        MoveAccuracyText.text = "000";
+        MoveTypeText.text = "";
+        MoveDescriptionText.text = "";
+
+        foreach (Transform child in MoveLayout)
+            Destroy(child.gameObject);
     }
 }
