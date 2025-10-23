a02b89092705b93e7397a83d69e7a94ef8779a6b
SA Omene <eserogheneo@gmail.com>
2025-10-23 09:07:17 -0500

    Update GameOverScene.cs
    Added position

--- Game Design/Scene/Scene Changes/GameOverScene.cs
+++ Game Design/Scene/Scene Changes/GameOverScene.cs
@@ -6,19 +6,21 @@ public class GameOverScene : Singleton<GameOverScene>
     [SerializeField] private Animator UIAnimator;
     [SerializeField] public TextMeshProUGUI GameOverText;
     [SerializeField] public string NextScene;
+    [SerializeField] private Vector3 Position = new Vector3(0, 0, 0);
 
     public void Start()
     {
         UIAnimator.Play("game_over_fade");
 
         //TODO: Test. Delete later
-        SetScene("Game Over\n Loser", "Start Scene");
+        // SetScene("Game Over\n Loser", "Start Scene");
     }
 
-    public void SetScene(string text, string sceneName)
+    public void SetScene(string text, string sceneName, Vector3 position)
     {
         GameOverText.text = text;
         NextScene = sceneName;
+        Position = position;
     }
 
     public void OnOKButtonPressed()
