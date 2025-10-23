0498d33e65e53dbe919b37e479d09692b8368618
SA Omene <eserogheneo@gmail.com>
2025-10-23 09:07:39 -0500

    Update BattleSimStatus.cs
    Added ability to check if player should be sent to game over screen after battle

--- Game Design/Battle/BattleSimStatus.cs
+++ Game Design/Battle/BattleSimStatus.cs
@@ -25,6 +25,7 @@ public class BattleSimStatus
     public static bool AfterRoundStarted;
     public static bool CanRun = true;
     public static bool RunSuccessful;
+    public static bool GameOverScreenIfLost;
     public static GameObject BurnSymbol;
     public static GameObject FrozenSymbol;
     public static GameObject PetrifiedSymbol;
@@ -87,6 +88,7 @@ public class BattleSimStatus
         AfterRoundStarted = false;
         RunSuccessful = false;
         CanRun = true;
+        GameOverScreenIfLost = false;
     }
 
     /// <summary>
