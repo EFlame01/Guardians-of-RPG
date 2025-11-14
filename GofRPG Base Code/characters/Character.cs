2a57ef938ab9859148839abf304f1ec6988a8f19
SA Omene <eserogheneo@gmail.com>
2025-11-14 09:08:16 -0600

    Update Character.cs

--- GofRPG Base Code/characters/Character.cs
+++ GofRPG Base Code/characters/Character.cs
@@ -3,27 +3,27 @@ using UnityEngine;
 ///<summary>
 /// Character is a class that
 /// holds all the character's basic information
-/// needed for the GofRPG_API.
+/// needed for the GofRPG Base Code.
 ///</summary>
 public class Character
 {
-    public string Id {get; protected set;}
-    public string Type {get; protected set;}
-    public string Name {get; protected set;}
-    public int Level {get; protected set;}
-    public int Bits {get; protected set;}
-    public Archetype Archetype {get; protected set;}
-    public string Sex {get; protected set;}
-    public Move[] BattleMoves {get; protected set;}
-    public BaseStats BaseStats {get; protected set;}
-    public BattleStatus BattleStatus {get; protected set;}
-    public Ability Ability {get; protected set;}
-    public Item Item {get; set;}
+    public string Id { get; protected set; }
+    public string Type { get; protected set; }
+    public string Name { get; protected set; }
+    public int Level { get; protected set; }
+    public int Bits { get; protected set; }
+    public Archetype Archetype { get; protected set; }
+    public string Sex { get; protected set; }
+    public Move[] BattleMoves { get; protected set; }
+    public BaseStats BaseStats { get; protected set; }
+    public BattleStatus BattleStatus { get; protected set; }
+    public Ability Ability { get; protected set; }
+    public Item Item { get; set; }
 
     //Constructor
     public Character(string id, string name, string type, int level, int bits, string archetype, string sex, Move[] battleMoves, int[] stats, Ability ability, Item item)
     {
-        Id = id; 
+        Id = id;
         Name = name;
         Type = type;
         Level = level;
@@ -43,7 +43,7 @@ public class Character
     {
 
     }
-    
+
     //Getters and Setters
     public void SetName(string name)
     {
@@ -55,7 +55,7 @@ public class Character
     }
     public void SetBits(int bits)
     {
-        Bits = bits < 0? 0 : bits;
+        Bits = bits < 0 ? 0 : bits;
     }
     public void SetArchetype(string archetype)
     {
@@ -67,7 +67,7 @@ public class Character
     }
     public void SetBattleMoves(string[] moves)
     {
-        for(int i = 0; i < 4; i++)
+        for (int i = 0; i < 4; i++)
             BattleMoves[i] = moves[i] != null ? MoveMaker.Instance.GetMoveBasedOnName(moves[i]) : null;
     }
     public void SetBaseStats(int fullHp, int atk, int def, int eva, int hp, int spd, double acc, double crt)
