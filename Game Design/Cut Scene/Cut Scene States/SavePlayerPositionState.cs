136877d92bd7227c94299bc4d0d598037c8b9c09
SA Omene <eserogheneo@gmail.com>
2025-11-25 23:21:31 -0600

    Created SavePlayerPositionState
    Created a CutSceneState that allows you to save the player's position to a specific location while in a cutscene.

---
+++ Game Design/Cut Scene/Cut Scene States/SavePlayerPositionState.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+public class SavePlayerPositionState : CutSceneState
+{
+    [SerializeField] public string sceneName;
+    [SerializeField] public string mapLocationName;
+    [SerializeField] public Vector3 playerPosition;
+
+    public override void Enter()
+    {
+        base.Enter();
+        SaveSystem.SavePlayerLocationData(sceneName, mapLocationName, playerPosition);
+        Exit();
+    }
+}
